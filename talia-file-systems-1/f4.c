b105aaec4279e32a87ff32fcd67a1457a2a68529
taliaxu09 <talia.minyin.xu@gmail.com>
2025-02-23 21:58:46 +1300

    .

--- talia-file-systems-1/f4.c
+++ talia-file-systems-1/f4.c
@@ -1,36 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 int main() {
-    FILE *file;
-    file = fopen("test.txt", "w+");
-    fputs("hello\n", file);
-    fputs("hello again\n", file);
-    fclose(file);
+    int fd1 = open("f4.txt", O_RDWR | O_CREAT | O_TRUNC);
 
-    file = fopen("test.txt", "a+");
+    write(fd1, "hello\n", strlen("hello\n"));
+	
     // Fork the process
     pid_t pid = fork();
-    
-    if (pid == 0) {
-        // Child process
-        fputs("abc\n", file);
-        fseek(file, 0, SEEK_SET);
-        char line[256];
-        if (fgets(line, sizeof(line), file) != NULL) {
-            printf("Child read: %s", line);
-        }
-    } else {
-        // Parent process
-        fseek(file, 0, SEEK_SET);
-        sleep(1);
-        char line[256];
-        if (fgets(line, sizeof(line), file) != NULL) {
-            printf("Parent read: %s", line);
-        }
-    }
 
+    if (pid == 0){
+		write(fd1, "hello again\n", strlen("hello again\n"));
+    } else {
+		sleep(0.1);
+		char line[256];
+		read(fd1, line, 256);
+		printf("%s", line);
+    }
     return 0;
 }
 
