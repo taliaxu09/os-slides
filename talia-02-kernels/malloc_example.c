a4737e372df40060d69e9a576306675d9f09a717
taliaxu09 <talia.minyin.xu@gmail.com>
2025-03-04 10:25:00 +1300

    .

---
+++ talia-02-kernels/malloc_example.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* functionA() {
+    // stack
+	/*
+	int num = 42; 
+    return &num;  
+	*/
+	
+	// heap
+	int* num = (int*) malloc(sizeof(int));
+    *num = 42;
+	return num;
+}
+
+int main() {
+    int* ptr = functionA();
+    printf("Stack Variable: %d\n", *ptr); 
+
+	free(ptr);
+	
+    return 0;
+}
