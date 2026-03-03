32486911ce9102b79133dddfbf808eea9e21d2f0
taliaxu09 <talia.minyin.xu@gmail.com>
2026-03-04 11:32:43 +1300

    .

---
+++ talia-02-kernels/swap2_pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void swap2(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int main(){
+    int a = 2, b = 3;
+    printf("Before swapping: %d %d\n", a, b);
+    swap2(&a, &b);
+    printf("After swapping: %d %d\n", a, b);   
+    return 0;
+}
